3114376c02f46e0bcb51bb5b7f2d9547c325bcf0
Tyler Sperle <tjsperle@madisoncollege.edu>
2021-02-01 15:08:59 -0600

    READY FOR GRADING - week2-script.py

---
+++ week2-script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+prints "Tyler Sperle" to the screen
+prints "I learned about GitHub" to the screen
+Usage: ./week2-script.py
+"""
+
+print("Tyler Sperle")
+print("I learned about GitHub")
+
