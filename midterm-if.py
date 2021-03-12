089eb611fd7631dbc1b8898adc19313393e438fd
Tyler Sperle <tjsperle@madisoncollege.edu>
2021-03-12 17:55:21 -0600

    midterm-if.py

--- midterm-if.py
+++ midterm-if.py
@@ -5,22 +5,19 @@ print("Name:Tyler Sperle")
 print("")
 
 Total = 0
-A = "gmeach18@ed.gov"
-B = "248.253.63.149"
 
-with open('Midterm-if.txt', 'r') as File:
-    List = File.readline()
-    try:
+keywords = ["gmeach18@ed.gov", "dcassyqw@microsoft.com", "Whiteland", "Kayley", 
+"248.253.63.149", "80.222.19.190"]
 
-        while True:
-            currentLine = next(File)
-            print(f"Line total is:{Total}")
-            Total += 1
+file = open("Midterm-if.txt", "r")
 
-        if line is A:
-            print(Total)
+data = file.readlines()
+for line in data:
+    for key in keywords:
 
-    except StopIteration:
+        if key in line:
+            words = line.split(",")
+            Total += int(words[0])
 
-        print(f"The total is: {Total}")
-        print(f"The total is: 2425")
+print(f"The total is: {Total}")
+file.close()
